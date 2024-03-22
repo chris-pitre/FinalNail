1dd09da4eb05786918826dc2048c17fdc82221c8
eggsandwich-dev <cpitre1@uncc.edu>
2024-03-22 19:27:03 -0400

    Initial Commit

---
+++ scenes/cell.gd
@@ -0,0 +1,9 @@
+class_name Cell
+extends Area3D
+
+@onready var top := $TopFace
+@onready var bottom := $BottomFace
+@onready var east := $EastFace
+@onready var west := $WestFace
+@onready var north := $NorthFace
+@onready var south := $SouthFace
